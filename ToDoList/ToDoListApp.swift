de0e17025f7a27ef325a0e6d4319c5ce0e4b97e6
Slav Kurochkin <kurochkinslav@gmail.com>
2024-06-22 22:15:23 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Slav Kurochkin on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
